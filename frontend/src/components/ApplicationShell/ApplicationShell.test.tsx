156e547a43576586a11915946a6c168b789d8abc
Nick Miller <nick@braid.co>
2021-08-22 12:41:25 -0700

    test cleanup

--- frontend/src/components/ApplicationShell/ApplicationShell.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ApplicationShell from './ApplicationShell';
-
-test('renders learn react link', () => {
-  render(<ApplicationShell />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
